9db6a516719530e90720fef12b0638195300fd85
richals <richalsthomask@gmail.com>
2022-06-09 13:15:55 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
+# About the project
+ 
+ ## Description 
+
+ This project lets you swipe swipe on the present component left and right to move the renderer to the component on the right and component on the left respectivly. You can set up an array of components to be selected back and forth.
+
+ ## Usage
+
+ components to be shown as the swipeable screen is written inside the project component as shown below.
+
+ 
+ import SwipeableScreens from 'react-swipeable-views-2'
+
+ <SwipeableScreens className="w-full">
+        
+         <div className="h-40 w-full bg-green-500">
+            slide number 1
+          </div>
+
+           <div className="h-40 w-full bg-red-500">
+            slide number 2
+          </div>
+
+           <div className="h-40 w-full bg-yellow-400">
+            slide number 3
+          </div>
+          
+           <div className="h-40 w-full bg-blue-500">
+            slide number 4
+          </div>
+
+ </SwipeableScreens>
+ 
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React, { useState } from "react";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function SwipeableScreens({
+  className,
+  style,
+  children,
+  currentSlide,
+  onChange,
+  drag_length = 80,
+}) {
+  const [touchStart, set_touchStart] = useState();
+  const [slide_ended, set_slide_ended] = useState(); //for slide ended in one direction wiggle effect
+
+  const swipingStyle = (cardNo, slide) => {
+    const slideStyle = children.map((_, slide) => {
+      return { transform: `translateX(${0 - slide * 100}%)` };
+    });
+
+    if (cardNo === slide) return slideStyle[slide];
+    else return { ...slideStyle[slide], display: "hidden" };
+  };
+
+  const handle_sliding = (end_index) => {
+    if (!touchStart) return;
+    if (
+      end_index - touchStart < -drag_length &&
+      currentSlide < children.length - 1
+    ) {
+      onChange((currentSlide) => currentSlide + 1);
+      set_touchStart();
+    } else if (end_index - touchStart > drag_length && currentSlide > 0) {
+      onChange((currentSlide) => currentSlide - 1);
+      set_touchStart();
+    }
+  };
+
+  return (
+    <div style={style} className={"flex flex-row overflow-x-hidden items-start " + className}>
+      {children.map((child, renderIndex) => (
+        <div
+          key={renderIndex}
+          className={
+            "flex flex-col min-w-full transition duration-300 border-white " +
+            (touchStart ? "cursor-pointer cursor-grabbing " : "") +
+            (slide_ended === "LEFT" ? "border-l-2 " : "") +
+            (slide_ended === "RIGHT" ? "border-r-2 " : "")
+          }
+          style={swipingStyle(renderIndex, currentSlide)}
+          //for touch screens
+          onTouchStart={(touchStartEvent) => {
+            set_touchStart(touchStartEvent.targetTouches[0].clientX);
+          }}
+          onTouchMove={(touchMoveEvent) => {
+            if (!touchStart) return;
+            if (
+              touchStart - touchMoveEvent.targetTouches[0].clientX >
+                drag_length ||
+              touchStart - touchMoveEvent.targetTouches[0].clientX <
+                -drag_length
+            )
+              handle_sliding(touchMoveEvent.targetTouches[0].clientX);
+            //for slide ended in one direction wiggle effect
+            if (
+              touchStart < touchMoveEvent.targetTouches[0].clientX &&
+              currentSlide <= 0
+            )
+              set_slide_ended("LEFT");
+            if (
+              touchStart > touchMoveEvent.targetTouches[0].clientX &&
+              currentSlide >= children.length - 1
+            )
+              set_slide_ended("RIGHT");
+          }}
+          onTouchEnd={() => {
+            set_slide_ended();
+            set_touchStart();
+          }}
+          //for mouse
+          onMouseDown={(mouseDownEvent) => {
+            set_touchStart(mouseDownEvent.screenX);
+          }}
+          onMouseMove={(mouseUpEvent) => {
+            if (!touchStart) return;
+            if (
+              touchStart - mouseUpEvent.screenX > drag_length ||
+              touchStart - mouseUpEvent.screenX < -drag_length
+            ) {
+              handle_sliding(mouseUpEvent.screenX);
+            }
+            //for slide ended in one direction wiggle effect
+            if (touchStart < mouseUpEvent.screenX && currentSlide <= 0)
+              set_slide_ended("LEFT");
+            if (
+              touchStart > mouseUpEvent.screenX &&
+              currentSlide >= children.length - 1
+            )
+              set_slide_ended("RIGHT");
+          }}
+          onMouseUp={() => {
+            set_slide_ended();
+            set_touchStart();
+          }}
+        >
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+}
+
